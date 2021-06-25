a7743882e4e8f322a492d9a714ba7e71415f4d3a
Huillca, Reynaldo <reynaldo.huillca@btgpactual.com>
2021-06-25 18:18:14 -0300

    feat: add returns with toString and valueOf

--- index.js
+++ index.js
@@ -28,3 +28,27 @@ console.assert(123 + '' === '123', 'implicit convertion to string')
 
 console.assert(('hello' || 123) === 'hello', '|| return always the first element if two elements are true')
 console.assert(('hello' && 123) === 123, '&& return always the last element if two elements are true')
+
+// ------------
+
+const item = {
+  name: 'DevOdla',
+  age: 27,
+  // string: 1 se nao for primitivo, chama o valueOf
+  toString() {
+    console.log('hey!')
+    return `Age: ${this.age}, Name: ${this.name}`
+  },
+  // number: 1 se nao for primitivo, chama o toString
+  valueOf() {
+    return { hey: 'dude' }
+    // return 007
+  }
+}
+
+console.log('item', item + 1)
+console.log('item', ''.concat(item))
+
+// convertion explicita
+console.log('toString', String(item))
+console.log('valueOf', Number(item))
